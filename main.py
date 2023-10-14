035cd339b29512c2218b4368d3985cda2a8b8676
Darren <45966937+djdunford@users.noreply.github.com>
2023-10-14 22:28:00 +0100

    uasyncio

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #from ws2812 import pixels_fill, pixels_show, color_chase, rainbow_cycle
 import ws2812
 import time
+import uasyncio
 
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -14,15 +15,20 @@ PURPLE = (180, 0, 255)
 WHITE = (255, 255, 255)
 COLORS = (BLACK, RED, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE)
 
-print("fills")
-for color in COLORS:       
-    ws2812.pixels_fill(color)
-    ws2812.pixels_show()
-    time.sleep(0.2)
+async def main():
+    print("fills")
+    for color in COLORS:       
+        ws2812.pixels_fill(color)
+        ws2812.pixels_show()
+        time.sleep(0.2)
 
-print("chases")
-for color in COLORS:       
-    ws2812.color_chase(color, 0.01)
+    print("chases")
+    for color in COLORS:       
+        ws2812.color_chase(color, 0.01)
 
-print("rainbow")
-ws2812.rainbow_cycle(0)
+    print("rainbow")
+    ws2812.rainbow_cycle(0)
+
+if __name__ == "__main__":
+    uasyncio.run(main())
+    
