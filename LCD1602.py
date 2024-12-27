bc75804904073d6ee8b275617088765b2e33a55f
Darren Dunford <45966937+djdunford@users.noreply.github.com>
2024-12-27 21:20:26 +0000

    LCD library

--- LCD1602.py
+++ LCD1602.py
@@ -2,14 +2,16 @@
 import time
 from machine import Pin,I2C
 
+# device I2C pins
 LCD1602_SDA = Pin(4)
 LCD1602_SCL = Pin(5)
 
 LCD1602_I2C = I2C(0,sda = LCD1602_SDA,scl = LCD1602_SCL ,freq = 400000)
 
-#Device I2C Arress
+# device I2C address
 LCD_ADDRESS   =  (0x7c>>1)
 
+# command
 LCD_CLEARDISPLAY = 0x01
 LCD_RETURNHOME = 0x02
 LCD_ENTRYMODESET = 0x04
@@ -19,13 +21,13 @@ LCD_FUNCTIONSET = 0x20
 LCD_SETCGRAMADDR = 0x40
 LCD_SETDDRAMADDR = 0x80
 
-#flags for display entry mode
+# flags for display entry mode
 LCD_ENTRYRIGHT = 0x00
 LCD_ENTRYLEFT = 0x02
 LCD_ENTRYSHIFTINCREMENT = 0x01
 LCD_ENTRYSHIFTDECREMENT = 0x00
 
-#flags for display on/off control
+# flags for display on/off control
 LCD_DISPLAYON = 0x04
 LCD_DISPLAYOFF = 0x00
 LCD_CURSORON = 0x02
@@ -33,13 +35,13 @@ LCD_CURSOROFF = 0x00
 LCD_BLINKON = 0x01
 LCD_BLINKOFF = 0x00
 
-#flags for display/cursor shift
+# flags for display/cursor shift
 LCD_DISPLAYMOVE = 0x08
 LCD_CURSORMOVE = 0x00
 LCD_MOVERIGHT = 0x04
 LCD_MOVELEFT = 0x00
 
-#flags for function set
+# flags for function set
 LCD_8BITMODE = 0x10
 LCD_4BITMODE = 0x00
 LCD_2LINE = 0x08
@@ -54,7 +56,6 @@ class LCD1602:
 
     self._showfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
     self.begin(self._row,self._col)
-
         
   def command(self,cmd):
     LCD1602_I2C.writeto_mem(LCD_ADDRESS, 0x80, chr(cmd))
@@ -72,6 +73,7 @@ class LCD1602:
   def clear(self):
     self.command(LCD_CLEARDISPLAY)
     time.sleep(0.002)
+
   def printout(self,arg):
     if(isinstance(arg,int)):
       arg=str(arg)
@@ -79,11 +81,9 @@ class LCD1602:
     for x in bytearray(arg,'utf-8'):
       self.write(x)
 
-
   def display(self):
     self._showcontrol |= LCD_DISPLAYON 
     self.command(LCD_DISPLAYCONTROL | self._showcontrol)
-
  
   def begin(self,cols,lines):
     if (lines > 1):
@@ -96,24 +96,18 @@ class LCD1602:
 
     # Send function set command sequence
     self.command(LCD_FUNCTIONSET | self._showfunction)
-    #delayMicroseconds(4500);  # wait more than 4.1ms
     time.sleep(0.005)
-    # second try
+
     self.command(LCD_FUNCTIONSET | self._showfunction);
-    #delayMicroseconds(150);
     time.sleep(0.005)
-    # third go
+
     self.command(LCD_FUNCTIONSET | self._showfunction)
-    # finally, set # lines, font size, etc.
+
     self.command(LCD_FUNCTIONSET | self._showfunction)
-    # turn the display on with no cursor or blinking default
+
     self._showcontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF 
     self.display()
-    # clear it off
     self.clear()
-    # Initialize to default text direction (for romance languages)
     self._showmode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT 
-    # set the entry mode
     self.command(LCD_ENTRYMODESET | self._showmode);
-    # backlight init
 
